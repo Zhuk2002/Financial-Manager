f6c348803d16b1cb26283ceb055a1ce79a6aacaf
Evgeniy <kharchenko_zhenya@bk.ru>
2024-11-21 17:35:29 +0700

    Add develop branch

--- FinancialManager/obj/Debug/net8.0/FinancialManager.AssemblyInfo.cs
+++ FinancialManager/obj/Debug/net8.0/FinancialManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinancialManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ee3e52a7ec22b116463836efa3d20b417904acb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50f6e9c97cb8f57777f6f0c06db810050d43f4a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinancialManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinancialManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
